6326b3804f6bd5d05e6df30d101e11ff840f2997
mulhern <amulhern@redhat.com>
2018-05-15 08:31:40 -0400

    Use Hypothesis to generate introspection data
    Ultimately, this is a much better approach than simply grabbing illustrative
    examples.
    
    Also, improve the tests for managed_object result. This allows to remove
    one nocover pragma for a RuntimeError, which is a good sign.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/test_generated.py
+++ tests/test_generated.py
@@ -2,15 +2,17 @@
 Test generation of class for invoking dbus methods.
 """
 
-import os
+import random
 import unittest
 
-import xml.etree.ElementTree as ET
+from hypothesis import given
 
 from dbus_client_gen import mo_query_builder
 from dbus_client_gen import managed_object_class
 
 from dbus_client_gen._errors import DbusClientRuntimeError
+
+from ._introspect import interface_strategy
 
 
 class TestCase(unittest.TestCase):
@@ -18,75 +20,73 @@ class TestCase(unittest.TestCase):
     Test the behavior of various auto-generated classes
     """
 
-    _DIRNAME = os.path.dirname(__file__)
+    @given(interface_strategy().map(lambda x: x.element()))
+    def test_managed_object(self, spec):
+        """
+        Test that the GMO object has the correct set of methods.
 
-    def setUp(self):
-        """
-        Read introspection data.
-        """
-        self._data = dict()
-        datadir = os.path.join(self._DIRNAME, "data")
-        for name in os.listdir(datadir):
-            if name[-4:] != '.xml':
-                continue
-            path = os.path.join(datadir, name)
-            with open(path) as opath:
-                self._data[name] = ET.fromstring("".join(opath.readlines()))
+        Test that there is an exception if the table is missing its interface.
 
-    def test_gmo_reader(self):
+        Test that there is an exception if the subtable is missing an entry.
         """
-        Test that GMO reader from interface spec has the correct methods.
+        interface_name = spec.attrib['name']
+        klass = managed_object_class(interface_name, spec)
+        property_names = [p.attrib['name'] for p in spec.findall("./property")]
+        self.assertTrue(all(hasattr(klass, name) for name in property_names))
 
-        Verify that empty table for an object always raises an exception.
-        """
-        for name, spec in self._data.items():
-            klass = managed_object_class(name, spec)
-            for prop in spec.findall("./property"):
-                name = prop.attrib['name']
-                self.assertTrue(hasattr(klass, name))
-                table = {spec.attrib['name']: {name: "tank"}}
-                obj = klass(table)
-                self.assertTrue(hasattr(obj, name))
-                self.assertEqual(getattr(obj, name)(), "tank")
+        with self.assertRaises(DbusClientRuntimeError):
+            klass({interface_name + "x": {}})
+
+        table = {
+            interface_name:
+            dict((name, random.randint(0, len(property_names)))
+                 for name in property_names)
+        }
+        table_interface = table[interface_name]
+        obj = klass(table)
+        self.assertTrue(
+            all(
+                getattr(obj, name)() == value
+                for (name, value) in table_interface.items()))
+
+        if table_interface != dict():
+            remove_name = random.choice([x for x in table_interface])
+            del table_interface[remove_name]
             with self.assertRaises(DbusClientRuntimeError):
-                klass(dict())
+                getattr(obj, remove_name)()
 
-    def test_gmo_query(self):
+    @given(interface_strategy().map(lambda x: x.element()))
+    def test_managed_object_query(self, spec):
         """
-        Test that gmo query builder returns a thing for an interface.
+        Test that the query returns appropriate values for its query input.
         """
-        for spec in self._data.values():
-            query = mo_query_builder(spec)
+        query = mo_query_builder(spec)
 
+        with self.assertRaises(DbusClientRuntimeError):
+            list(query(dict(), {"bogus": None}))
+
+        properties = [p.attrib['name'] for p in spec.findall("./property")]
+        name = spec.attrib['name']
+        table = {
+            'junk': {
+                name: dict((k, None) for k in properties)
+            },
+            'other': {
+                "interface": dict()
+            },
+            'nomatch': {
+                name: dict((k, 2) for k in properties)
+            },
+        }
+
+        if properties:
+            self.assertEqual(
+                len(list(query(table, dict((k, None) for k in properties)))),
+                1)
             with self.assertRaises(DbusClientRuntimeError):
-                list(query(dict(), {"bogus": None}))
-
-            properties = [p.attrib['name'] for p in spec.findall("./property")]
-            name = spec.attrib['name']
-            table = {
-                'junk': {
-                    name: dict((k, None) for k in properties)
-                },
-                'other': {
-                    "interface": dict()
-                },
-                'nomatch': {
-                    name: dict((k, 2) for k in properties)
-                },
-            }
-
-            if properties:
-                self.assertEqual(
-                    len(
-                        list(
-                            query(table, dict(
-                                (k, None) for k in properties)))), 1)
-                with self.assertRaises(DbusClientRuntimeError):
-                    table = {"junk": {name: dict()}}
-                    list(query(table, dict((k, None) for k in properties)))
-            else:
-                self.assertEqual(
-                    len(
-                        list(
-                            query(table, dict(
-                                (k, None) for k in properties)))), 2)
+                table = {"junk": {name: dict()}}
+                list(query(table, dict((k, None) for k in properties)))
+        else:
+            self.assertEqual(
+                len(list(query(table, dict((k, None) for k in properties)))),
+                2)
