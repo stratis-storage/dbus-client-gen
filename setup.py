d51de3607c81d8e9135bd87d585bb1f620eddc5c
mulhern <amulhern@redhat.com>
2023-02-27 21:19:29 -0500

    Make newer style package target and run in weekly tests
    Signed-off-by: mulhern <amulhern@redhat.com>

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setuptools.setup(
     author_email="amulhern@redhat.com",
     description="generates classes and methods useful to a D-Bus client",
     long_description=long_description,
+    long_description_content_type="text/x-rst",
     platforms=["Linux"],
     license="MPL-2.0",
     classifiers=[
